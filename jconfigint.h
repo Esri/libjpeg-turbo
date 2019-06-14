928596f32309c7eba703ff523c86ff95c490297a
Colin Powell <cpowell@esri.com>
2019-06-13 18:13:57 -0700

    libjpeg-turbo: combine build configs
    Create a single jconfig.h and jconfigint.h that are
    the union of all versions of these files generated
    for all supported platforms. Files were generated
    using the cmake scripts documented in BUILDING.MD.
    
    Config files for Windows platforms differed slightly than those
    produced for Unix-y platforms (iOS, MacOS, Linux, Android);
    `#if(defined _WIN32)` blocks were used where necessary
    to specify Windows-only settings.
    
    The value of SIZE_OF_SIZE_T differed between 32-bit and 64-bit
    build configs; the #ifdef blocks used here to set this value
    appropriately are adapted from the libjpeg-turbo config files
    located in the skia library.
    
    Signed-off by: Colin Powell <cpowell@esri.com>

--- jconfigint.h
+++ jconfigint.h
@@ -1,24 +1,14 @@
-/* jconfigint.h.  Customized for android on the basis of jconfigint.h.in. */
+/* libjpeg-turbo build number */
+#define BUILD "20190613"
 
-#ifndef __JCONFIGINT_H__
-#define __JCONFIGINT_H__
-
-
-#define BUILD ""
+/* Compiler's inline keyword */
+#undef inline
 
 /* How to obtain function inlining. */
-#ifndef INLINE
-  #ifndef TURBO_FOR_WINDOWS
-    #define INLINE inline __attribute__((always_inline))
-  #else
-    #if defined(__GNUC__)
-      #define INLINE inline __attribute__((always_inline))
-    #elif defined(_MSC_VER)
-      #define INLINE __forceinline
-    #else
-      #define INLINE
-    #endif
-  #endif
+#if defined(_WIN32)
+#define INLINE __forceinline
+#else
+#define INLINE  __inline__ __attribute__((always_inline))
 #endif
 
 /* Define to the full name of this package. */
@@ -30,7 +20,8 @@
 /* The size of `size_t', as reported by the compiler through the
  * builtin macro __SIZEOF_SIZE_T__. If the compiler does not
  * report __SIZEOF_SIZE_T__ add a custom rule for the compiler
- * here. */
+ * here. Adapted from libjpeg-turbo config files for the skia 
+ * library. */
 #ifdef __SIZEOF_SIZE_T__
 #define SIZEOF_SIZE_T __SIZEOF_SIZE_T__
 #elif __WORDSIZE==64 || defined(_WIN64)
@@ -39,4 +30,20 @@
 #define SIZEOF_SIZE_T 4
 #endif
 
-#endif // __JCONFIGINT_H__
+/* Define if your compiler has __builtin_ctzl() and sizeof(unsigned long) == sizeof(size_t). */
+#if !defined(_WIN32)
+#define HAVE_BUILTIN_CTZL
+#endif
+
+/* Define to 1 if you have the <intrin.h> header file. */
+#if defined(_WIN32)
+#define HAVE_INTRIN_H
+#endif
+
+#if defined(_MSC_VER) && defined(HAVE_INTRIN_H)
+#if (SIZEOF_SIZE_T == 8)
+#define HAVE_BITSCANFORWARD64
+#elif (SIZEOF_SIZE_T == 4)
+#define HAVE_BITSCANFORWARD
+#endif
+#endif
